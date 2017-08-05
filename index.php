8d89766f67a0d9f5c935d6d611805fd69ee1637c
Bishakha Nanda <nanda.bishakha@gmail.com>
2017-08-05 18:10:45 +0530

    Initial project release

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<title>Easy HRM - A Simple HRM Solution</title>
+		<link href="assets/css/custom.css" type="text/css" rel="stylesheet" />
+	</head>
+	
+	<body>
+	
+		<header class="full-width clearfix">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12 col-md-12 text-center">
+						<h1>A simple HTML layout for our HRM</h1>
+					</div>
+				</div>
+			</div>
+		</header>
+	
+	</body>
+
+</html>
